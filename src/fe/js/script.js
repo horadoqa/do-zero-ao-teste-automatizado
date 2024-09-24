824219d42fe7bc4c513d63c3a264ee572ca074cb
Ricardo Fahham <rfahham@hotmail.com>
2024-09-24 12:10:55 -0300

    adicionando validação no FrontEnd

--- src/fe/js/script.js
+++ src/fe/js/script.js
@@ -1,18 +1,53 @@
 document.addEventListener('DOMContentLoaded', function() {
     console.log("Script carregado!");
 
-    document.getElementById('submitButton').addEventListener('click', function(event) {
-        event.preventDefault(); // Evita o envio do formulário
-        
-        console.log("Botão acionado !!!"); // Log quando o botão é clicado
-        this.textContent = 'Enviado'; // Muda o texto do botão
-        this.disabled = true; // Desabilita o botão após o clique
+    const submitButton = document.getElementById('submitButton');
+    
+    const requiredFields = [
+        document.getElementById('name'),
+        document.getElementById('email'),
+        document.getElementById('phone'),
+        document.getElementById('country'),
+        document.getElementById('select-itens')
+    ];
 
-        // Aqui você pode adicionar qualquer lógica adicional, como validação de formulário ou exibir uma mensagem de sucesso.
-        // Exemplo:
-        // alert('Seu formulário foi enviado!');
+    // Função para verificar se todos os campos obrigatórios estão preenchidos
+    function checkFormValidity() {
+        const allFilled = requiredFields.every(field => field.value.trim() !== '');
+        submitButton.disabled = !allFilled; // Habilita ou desabilita o botão
+    }
 
-        // Redireciona para a segunda página
-        window.location.href = "obrigado.html"; // Altere para o caminho correto da sua página de destino
+    // Adiciona eventos de input para todos os campos obrigatórios
+    requiredFields.forEach(field => {
+        field.addEventListener('input', checkFormValidity);
     });
+
+    // Inicialmente desabilita o botão se os campos não estiverem preenchidos
+    checkFormValidity();
+
+    submitButton.addEventListener('click', async function(event) {
+        event.preventDefault();
+
+        const email = document.getElementById('email').value.trim();
+       
+        // Validações simples
+        if (!validateEmail(email)) {
+            alert('Por favor, insira um e-mail válido.');
+            return;
+        }
+
+        // Muda o texto do botão e desabilita
+        console.log("Botão acionado !!!");
+        this.textContent = 'Enviado';
+        this.disabled = true;
+
+        window.location.href = "http://127.0.0.1:5500/src/fe/obrigado.html";
+
+    });
+
+    // Função para validar e-mail
+    function validateEmail(email) {
+        const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+        return re.test(email);
+    }
 });
